2e35cb57102cc6ac4516a7bbf5f2c2088cc7bbaf
Yixin <52696292+Dereck0602@users.noreply.github.com>
2024-09-06 22:07:16 +0800

    Update run_llama3.sh

--- DSnoT/run_llama3.sh
+++ DSnoT/run_llama3.sh
@@ -1,16 +1,29 @@
-#model='/ossfs/workspace/common_base_model/Meta-Llama-3-8B-Instruct'
-model='/ossfs/workspace/datacube-nas/yixin_llm/Meta-Llama-3-8B'
+model='/ossfs/workspace/common_base_model/Meta-Llama-3-8B-Instruct'
+data='slimpajama'
+output_file="llama3_8b_instruct_${data}_results.txt"
 
-CUDA_VISIBLE_DEVICES=1 python main.py \
-    --model $model \
-    --prune_method DSnoT \
-    --seed 10 \
-    --initial_method wanda \
-    --model_type llama \
-    --nsamples 1024 \
-    --sparsity_ratio 0.5 \
-    --sparsity_type 4:8 \
-    --max_cycle_time 50 \
-    --update_threshold 0.1 \
-    --pow_of_var_regrowing 1 \
-    --save_model '/mntnlp/yixin.jyx/yixin.jyxx/yixin.jyx/output/llama3-8b-DSnoT-wanda-4:8-dclm-seed10'
+
+# 清空输出文件
+> $output_file
+
+for seed in {0..19}; do
+    seed_value=$((seed * 5))
+    echo "Running with seed: $seed" >> $output_file
+    echo "Running with calibration data: $data" >> $output_file
+    CUDA_VISIBLE_DEVICES=1 python main.py \
+        --model $model \
+        --prune_method DSnoT \
+        --seed $seed_value \
+        --data $data \
+        --initial_method wanda \
+        --model_type llama \
+        --nsamples 1024 \
+        --sparsity_ratio 0.5 \
+        --sparsity_type 4:8 \
+        --max_cycle_time 50 \
+        --update_threshold 0.1 \
+        --pow_of_var_regrowing 1 \
+        --eval_benchmark >> $output_file 2>&1
+    echo "Finished running with seed: $seed" >> $output_file
+    echo "------------------------" >> $output_file
+done
