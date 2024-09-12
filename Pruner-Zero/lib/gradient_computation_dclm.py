f7ac69817e558df8ae3a788c180f60687558294c
Yixin <52696292+Dereck0602@users.noreply.github.com>
2024-09-12 12:59:08 +0800

    Update gradient_computation_dclm.py

--- Pruner-Zero/lib/gradient_computation_dclm.py
+++ Pruner-Zero/lib/gradient_computation_dclm.py
@@ -192,6 +192,37 @@ def get_wikipedia(nsamples, seed, seqlen, tokenizer):
     valenc = TokenizerWrapper(valenc)
     return trainloader, valenc
 
+def get_regenc4(nsamples, seed, seqlen, tokenizer):
+    # Load train and validation datasets
+    traindata = load_dataset('json',data_files='/ossfs/workspace/yixin.jyx/data/dclm_train_llama3_8b_256_reppenalty.json')['train']
+    valdata = load_from_disk('/ossfs/workspace/datacube-nas/yixin_llm/data/c4_validation')
+    
+    trainenc = tokenizer(' '.join(traindata['text']), return_tensors='pt')
+
+    # Generate samples from training set
+    random.seed(seed)
+    trainloader = []
+    for _ in range(nsamples):
+        '''
+        while True:
+            i = random.randint(0, len(traindata) - 1)
+            trainenc = tokenizer(traindata[i]['text'], return_tensors='pt')
+            if trainenc.input_ids.shape[1] > seqlen:
+                break
+        '''
+        i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
+        j = i + seqlen
+        inp = trainenc.input_ids[:, i:j]
+        tar = inp.clone()
+        tar[:, :-1] = -100
+        trainloader.append((inp, tar))
+
+    # Prepare validation dataset
+    valenc = tokenizer(' '.join(valdata[:1100]['text']), return_tensors='pt')
+    valenc = valenc.input_ids[:, :(256 * seqlen)]
+    valenc = TokenizerWrapper(valenc)
+    return trainloader, valenc
+
 # Function to select the appropriate loader based on dataset name
 def get_loaders(name, nsamples=128, seed=0, seqlen=2048, tokenizer=None):
     if 'wikitext2' in name:
@@ -204,6 +235,8 @@ def get_loaders(name, nsamples=128, seed=0, seqlen=2048, tokenizer=None):
         return get_slimpajama(nsamples, seed, seqlen, tokenizer)
     if 'wikipedia' in name:
         return get_wikipedia(nsamples, seed, seqlen, tokenizer)
+    if 'regen' in name:
+        return get_regenc4(nsamples, seed, seqlen, tokenizer)
 
 
 def get_llm(model, cache_dir='llm_weights'):
