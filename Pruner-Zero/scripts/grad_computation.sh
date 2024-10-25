40414d4f6a82881dfb4e1d30728ae95b7b3cfff5
Yixin <52696292+Dereck0602@users.noreply.github.com>
2024-10-25 15:39:00 +0800

    Delete Pruner-Zero directory

--- Pruner-Zero/scripts/grad_computation.sh
+++
@@ -1,4 +0,0 @@
-# llama-2-7b
-LLAMA2_PATH='/ossfs/workspace/common_base_model/Meta-Llama-3-8B-Instruct'
-CUDA_VISIBLE_DEVICES=2 python lib/gradient_computation.py --nsamples 128 \
-    --model $LLAMA2_PATH --llama_version 3 --task gradient
