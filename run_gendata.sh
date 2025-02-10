6c2200e939b16c1fc8cc57ccc251265da23a1d36
Yixin <52696292+Dereck0602@users.noreply.github.com>
2025-02-10 14:58:30 +0800

    Update run_gendata.sh

--- run_gendata.sh
+++ run_gendata.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=0
 #model='path/Meta-Llama-3-8B'
 model="path/DCLM-7B"
-python generate_dclm.py \
+python generate_data.py \
     --base_model $model \
     --data_path 'path/wikipedia_5k.json' \
